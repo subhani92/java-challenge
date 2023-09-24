18b2e1eab2bb986629355f06c332733b18be6c02
ts-fareed.subhani <ts-fareed.subhani@rakuten.com>
2023-09-24 11:59:18 +0900

    - adding caching policy in the get delete and update method

--- src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
+++ src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
@@ -3,6 +3,8 @@ package jp.co.axa.apidemo.controllers;
 import jp.co.axa.apidemo.entities.Employee;
 import jp.co.axa.apidemo.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +21,7 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees")
+    @Cacheable("employees")
     public List<Employee> getEmployees() {
         List<Employee> employees = employeeService.retrieveEmployees();
         return employees;
@@ -36,19 +39,23 @@ public class EmployeeController {
     }
 
     @DeleteMapping("/employees/{employeeId}")
+    @CacheEvict(value = "employees", allEntries = true) // Clear cache on delete
     public void deleteEmployee(@PathVariable(name="employeeId")Long employeeId){
         employeeService.deleteEmployee(employeeId);
         System.out.println("Employee Deleted Successfully");
     }
 
     @PutMapping("/employees/{employeeId}")
+    @CacheEvict(value = "employees", allEntries = true) // Clear cache on update
     public void updateEmployee(@RequestBody Employee employee,
-                               @PathVariable(name="employeeId")Long employeeId){
+                               @PathVariable(name="employeeId")Long employeeId) {
         Employee emp = employeeService.getEmployee(employeeId);
         if(emp != null){
             employeeService.updateEmployee(employee);
         }
-
+        else {
+            System.out.print("Employee not found with ID: " + employeeId);
+        }
     }
 
 }
